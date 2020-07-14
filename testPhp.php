2b9ef3e09ee8eecfbe73c59cf6f90bbbff1e3bcc
Liora Telouk <halfonliora@gmail.com>
2020-07-14 23:58:44 +0200

    set up search function

--- testPhp.php
+++ testPhp.php
@@ -1,3 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title></title>
+</head>
+<body>
+
+	<form method="GET">
+	   <input type="search" name="q" placeholder="Recherche..." />
+	   <input type="submit" value="Valider" />
+	</form>
+
+</body>
+</html>
 
 <?php
 	$url = "https://api.androidhive.info/json/movies.json";
@@ -11,4 +26,28 @@
 	   });
 	}
 ?>
+
+<?php if($articles && count($articles) > 0) { ?>
+   <ul>
+		<?php 
+		/*
+		while($a = $articles->fetch()) { ?>
+		  <li><?= $a['titre'] ?></li>
+		<?php 
+		} */
+
+		foreach ($articles as $a) { ?>
+		  <li><?php echo $a['title'] ?> <br/>
+          <?php
+            $image = $a['image'] ;
+            print '<img src="'.$image.'" width="200" height="150" alt="texte alternatif" />';?> <br/>
+		  <?php echo $a['rating'] ?> /10 <br/>
+		  <?php echo $a['releaseYear'] ?> </li> <br/>
+		<?php 
+		}
+		?>
+   </ul>
+<?php } else { ?>
+	Aucun résultat pour: <?= $q ?>...
+<?php } ?>
 
